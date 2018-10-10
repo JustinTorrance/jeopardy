141f4f39eb6701424670c7102b51ad9f376e02dd
Cody-Price <ctprice11@gmail.com>
2018-10-10 14:11:15 -0600

    Add player class with basic structure to be filled in later

---
+++ js/classes/player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(round, players, gameState, playerName, score) {
+    super(round, players, gameState);
+    this.playerName = playerName;
+    this.score = score;
+  }
+
+  submitAnswer() {
+
+  }
+
+  submitWager() {
+
+  }
+
+  updateScore() {
+
+  }
+}
