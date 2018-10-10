2f75e2d00dd4f625a86988a8d2fe1eb5c6e48ff6
Justin Torrance <justin.torrance91@gmail.com>
2018-10-10 14:02:33 -0600

    Create game class

--- js/classes/game.js
+++ js/classes/game.js
@@ -1,3 +1,32 @@
 class Game {
-  
+  constructor(round, players, gameState) {
+    this.round = round;
+    this.players = players;
+    this.gameState = gameState;
+  }
+
+  startGame() {
+
+  }
+
+  quitGame() {
+
+  }
+
+  incrementRoud() {
+
+  }
+
+  declareWinner() {
+
+  }
+
+  rollDice() {
+
+  }
+
+  newTextPrompt(prompt) {
+    return prompt;
+  }
+
 }
