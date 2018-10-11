d573215a1bb2cb627a1cdb9bb9edb22e3993a946
Justin Torrance <justin.torrance91@gmail.com>
2018-10-11 14:15:33 -0600

    Move dom methods to domUpdates

--- js/classes/game.js
+++ js/classes/game.js
@@ -5,26 +5,26 @@ class Game {
     this.gameState = gameState;
   }
 
-  appendNamesAndScores(playerOne, playerTwo, playerThree) {
-      document.querySelector('.player-one-name').innerHTML = playerOne.playerName;
-      document.querySelector('.player-two-name').innerHTML = playerTwo.playerName;
-      document.querySelector('.player-three-name').innerHTML = playerThree.playerName;
-      document.querySelector('.player-one-score').innerHTML = playerOne.score;
-      document.querySelector('.player-two-score').innerHTML = playerTwo.score;
-      document.querySelector('.player-three-score').innerHTML = playerThree.score;
-  };
+  // appendNamesAndScores(playerOne, playerTwo, playerThree) {
+  //     document.querySelector('.player-one-name').innerHTML = playerOne.playerName;
+  //     document.querySelector('.player-two-name').innerHTML = playerTwo.playerName;
+  //     document.querySelector('.player-three-name').innerHTML = playerThree.playerName;
+  //     document.querySelector('.player-one-score').innerHTML = playerOne.score;
+  //     document.querySelector('.player-two-score').innerHTML = playerTwo.score;
+  //     document.querySelector('.player-three-score').innerHTML = playerThree.score;
+  // };
 
   populateGameBoard() {
     
   }
 
-  startGame() {
-    playerOne = new Player(this.round, undefined, this.gameState, undefined, 0, 1);
-    playerTwo = new Player(this.round, undefined, this.gameState, undefined, 0, 2);
-    playerThree = new Player(this.round, undefined, this.gameState, undefined, 0, 3);
-    this.appendNamesAndScores(playerOne, playerTwo, playerThree);
-    populateGameBoard()
-  }
+  // startGame() {
+  //   playerOne = new Player(this.round, undefined, this.gameState, undefined, 0, 1);
+  //   playerTwo = new Player(this.round, undefined, this.gameState, undefined, 0, 2);
+  //   playerThree = new Player(this.round, undefined, this.gameState, undefined, 0, 3);
+  //   this.appendNamesAndScores(playerOne, playerTwo, playerThree);
+  //   populateGameBoard()
+  // }
 
   quitGame() {
 
